1455089be3ea12525f176f93ad76900a10006732
Scorpion-g <matteo69riviere@gmail.com>
2024-09-21 23:05:44 +0200

    modified:   src/events/interactionCreate/handleCommands.js 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,39 +1,33 @@
 require('dotenv').config();
 const { Client, IntentsBitField, ActivityType } = require('discord.js');
-const { CommandHandler } = require('djs-commander')
+const { CommandHandler } = require('djs-commander');
 const mongoose = require('mongoose');
-const eventHandler= require('./handlers/eventHandler'); 
-const path = require('path')
+const eventHandler = require('./handlers/eventHandler');
+const path = require('path');
 const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.GuildPresences,
-        IntentsBitField.Flags.MessageContent,
-    ]
+	intents: [
+		IntentsBitField.Flags.Guilds,
+		IntentsBitField.Flags.GuildMembers,
+		IntentsBitField.Flags.GuildMessages,
+		IntentsBitField.Flags.GuildPresences,
+		IntentsBitField.Flags.MessageContent,
+	],
 });
 new CommandHandler({
-    client,
-    commandsPath: path.join(__dirname, 'commands/subcommands')
+	client,
+	commandsPath: path.join(__dirname, 'commands/subcommands'),
 });
 
-
-
 (async () => {
-    try {
-    mongoose.set('strictQuery', false);
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log('Connected to DB.');
+	try {
+		mongoose.set('strictQuery', false);
+		await mongoose.connect(process.env.MONGODB_URI);
+		console.log('Connected to DB.');
 
-    eventHandler(client);
+		eventHandler(client);
 
-    client.login(process.env.TOKEN);
-    } catch (error) {
-    console.log(`Error: ${error}`);
-    }
+		client.login(process.env.TOKEN);
+	} catch (error) {
+		console.log(`Error: ${error}`);
+	}
 })();
-
-
-
-
