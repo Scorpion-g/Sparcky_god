adb016f47a2882b56e5c3df0b68193015f227d0e
Scorpion-g <matteo69riviere@gmail.com>
2024-09-21 23:02:00 +0200

    #src/commands/subcommands/ping.js

--- src/commands/subcommands/ping.js
+++ src/commands/subcommands/ping.js
@@ -1,11 +1,9 @@
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription('Pong!'),
-        
-    run: ({ interaction, client, handler }) => {
-    interaction.reply(`Pong! ${client.ws.ping}ms`);
-    },
+	data: new SlashCommandBuilder().setName('ping').setDescription('Pong!'),
+
+	run: ({ interaction, client, handler }) => {
+		interaction.reply(`Pong! ${client.ws.ping}ms`);
+	},
 };
