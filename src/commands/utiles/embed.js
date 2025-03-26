e34543f9be502caf40cb9f157bf93b68741a9f24
Mattéo <scorpion@archlinux.localdomain>
2025-03-26 20:07:42 +0100

    Change gitignore

--- src/commands/utiles/embed.js
+++ src/commands/utiles/embed.js
@@ -1,48 +1,43 @@
-const { EmbedBuilder, ApplicationCommandOptionType } = require('discord.js');
+const { EmbedBuilder, ApplicationCommandOptionType } = require("discord.js");
 module.exports = {
     /**
      *
      *
      * @param {Client} client
      * @param {Interaction} interaction
-     * 
      *
-     */ 
-	name: 'embed',
-	description: 'Créer un embed',
-	options: [
-		{
-			name: 'titre',
-			description: 'ajouter un titre',
-			type: ApplicationCommandOptionType.String,
-			required: true,
-		},
-		{
-			name: 'description',
-			description: 'ajouter une descritpion',
-			type: ApplicationCommandOptionType.String,
-			required: true,
-		},
-	],
-	//devOnly: Boolean,
-	//testOnly:Boolean,
-	//options: Object[],
+     *
+     */
+    name: "embed",
+    description: "Créer un embed",
+    options: [
+        {
+            name: "titre",
+            description: "ajouter un titre",
+            required: true,
+            type: ApplicationCommandOptionType.String,
+        },
+        {
+            name: "description",
+            description: "ajouter une descritpion",
+            required: true,
+            type: ApplicationCommandOptionType.String,
+        },
+    ],
+    //devOnly: Boolean,
+    //testOnly:Boolean,
+    //options: Object[],
 
-	callback: async (client, interaction) => {
-		const title = interaction.options.get('titre').value;
-		
-		const description = interaction.options.get('description').value;
-		const author = interaction.author.value
-		try {
-			const embed = new EmbedBuilder()
-				.setColor('#0099ff')
-				.setTitle(`${title}`)
-				.setDescription(`${description}`)
-			
+    callback: async (client, interaction) => {
+        const title = interaction.options.get("titre").value;
+        const description = interaction.options.get("description").value;
+        const author = interaction.author.value;
+        try {
+            const embed = new EmbedBuilder().setColor("#0099ff");
 
-			interaction.reply({ embeds: [embed] });
-		} catch (error) {
-			console.log(`Il y a une erreur: ${error}`);
-		}
-	},
+            interaction.reply({ embeds: [embed] });
+        } catch (error) {
+            console.log(`Il y a une erreur dans embed: ${error}`);
+        }
+    },
 };
--- src/subcommands/ping.js
+++ src/subcommands/ping.js
@@ -1,10 +1,10 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-	deleted:true,
-	data: new SlashCommandBuilder().setName('ping').setDescription('Pong!'),
+    deleted: false,
+    data: new SlashCommandBuilder().setName("ping").setDescription("Pong!"),
 
-	run: ({ interaction, client, handler }) => {
-		interaction.reply(`Pong! ${client.ws.ping}ms`);
-	},
+    run: ({ interaction, client, handler }) => {
+        interaction.reply(`Pong! ${client.ws.ping}ms`);
+    },
 };
