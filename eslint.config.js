b4a69f29e98d1a5d0cf59642a19e5ece34b4148e
Scorpion-g <matteo69riviere@gmail.com>
2025-09-23 19:37:54 +0200

    Refactor ESLint configuration to use array format

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,28 @@
 // eslint.config.js
 const js = require('@eslint/js');
 
-module.exports = {
-  ...js.configs.recommended,
-  ignores: ['node_modules/**'],
-  env: {
-    node: true,
-    es2021: true,
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      globals: {
+        // Node.js global variables
+        process: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        exports: "readonly",
+        require: "readonly",
+        module: "readonly",
+        Buffer: "readonly",
+        global: "readonly",
+        // Add more Node.js globals as needed
+      },
+    },
+    ignores: ['node_modules/**'],
+    rules: {
+      // Your custom rules
+    },
   },
-  // Add your custom rules here
-};
+];
