139acb4d123d767df494e6fadc0ad28b711bd9a8
Scorpion-g <matteo69riviere@gmail.com>
2025-09-23 19:34:14 +0200

    Refactor ESLint config to use object syntax

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,12 @@
-// eslint.config.cjs
+// eslint.config.js
 const js = require('@eslint/js');
 
-module.exports = [
-  js.configs.recommended,
-  {
-    ignores: ['node_modules/**'],
-    // Add your custom rules here
-  },
+module.exports = {
+  ...js.configs.recommended,
+  ignores: ['node_modules/**'],
   env: {
     node: true,
     es2021: true,
   },
-  // 
-];
+  // Add your custom rules here
+};
